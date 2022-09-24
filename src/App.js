cb83247ab55dbae5dd82184deb1709887452eadb
mahbubnoyon506 <mahbubnoyon506@gmail.com>
2022-09-24 14:02:58 +0600

    router setup complete

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
-import { Button } from '@mui/material';
+import { Route, Routes } from 'react-router-dom';
+import Home from './Pages/Home/Home';
 
 function App() {
   return (
     <div>
-      <Button variant="contained">Hello World</Button>
+      <Routes>
+        <Route path="/" element={<Home />} />
+      </Routes>
     </div>
   );
 }
