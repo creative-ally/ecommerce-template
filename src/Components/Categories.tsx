a911dab2089c37e377d416945c8614c9539778fc
Neamul01 <neamulhaque2002@gmail.com>
2022-10-15 11:38:45 +0600

    component intialized

---
+++ src/Components/Categories.tsx
@@ -0,0 +1,28 @@
+import { Box, Container, Stack } from '@mui/material';
+import React from 'react';
+
+const Categories = () => {
+    return (
+        <Container>
+
+        </Container>
+    );
+};
+
+export default Categories;
+
+const categories = [
+    {
+        id: 1,
+        name: 'sofa',
+        icon: "SofaIcon"
+    }, {
+        id: 2,
+        name: 'sofa',
+        icon: "SofaIcon"
+    }, {
+        id: 3,
+        name: 'sofa',
+        icon: "SofaIcon"
+    },
+]
