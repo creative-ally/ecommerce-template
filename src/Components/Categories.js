3cd3676944244ab70fe1f59ab6641576309acb5e
Neamul01 <neamulhaque2002@gmail.com>
2022-10-15 13:22:49 +0600

    card added with problems

---
+++ src/Components/Categories.js
@@ -0,0 +1,183 @@
+import { Box, Card, CardActionArea, CardMedia, Container, Grid, Stack, Typography } from '@mui/material';
+import React from 'react';
+import ChairIcon from '@mui/icons-material/Chair';
+import MuiIcon from './MuiIcon';
+import CategoriCard from './CategoriCard';
+import { Chair } from '@mui/icons-material';
+
+
+const Categories = () => {
+    return (
+        <Container maxWidth='lg' sx={{
+            my: '2rem'
+        }}>
+            <Stack container direction={'row'}
+                sx={{
+                    flexWrap: 'wrap'
+                }}
+            >
+                {/* sofa card  */}
+                <Card sx={{
+                    maxWidth: '8.87rem',
+                    minWidth: '8.5rem',
+                    display: 'flex',
+                    flexDirection: 'column',
+                    alignItems: 'center'
+                }}>
+                    <Chair sx={{ fontSize: '3rem' }} />
+                    <Typography component={'h3'} sx={{
+                        fontWeight: 'bold'
+                    }}>
+                        Sofa
+                    </Typography>
+                </Card>
+
+                {/* sofa card  */}
+                <Card sx={{
+                    maxWidth: '8.87rem',
+                    minWidth: '8.5rem',
+                    display: 'flex',
+                    flexDirection: 'column',
+                    alignItems: 'center'
+                }}>
+                    <Chair sx={{ fontSize: '3rem' }} />
+                    <Typography component={'h3'} sx={{
+                        fontWeight: 'bold'
+                    }}>
+                        Sofa
+                    </Typography>
+                </Card>
+
+                {/* sofa card  */}
+                <Card sx={{
+                    maxWidth: '8.87rem',
+                    minWidth: '8.5rem',
+                    display: 'flex',
+                    flexDirection: 'column',
+                    alignItems: 'center'
+                }}>
+                    <Chair sx={{ fontSize: '3rem' }} />
+                    <Typography component={'h3'} sx={{
+                        fontWeight: 'bold'
+                    }}>
+                        Sofa
+                    </Typography>
+                </Card>
+
+                {/* sofa card  */}
+                <Card sx={{
+                    maxWidth: '8.87rem',
+                    minWidth: '8.5rem',
+                    display: 'flex',
+                    flexDirection: 'column',
+                    alignItems: 'center'
+                }}>
+                    <Chair sx={{ fontSize: '3rem' }} />
+                    <Typography component={'h3'} sx={{
+                        fontWeight: 'bold'
+                    }}>
+                        Sofa
+                    </Typography>
+                </Card>
+
+                {/* sofa card  */}
+                <Card sx={{
+                    maxWidth: '8.87rem',
+                    minWidth: '8.5rem',
+                    display: 'flex',
+                    flexDirection: 'column',
+                    alignItems: 'center'
+                }}>
+                    <Chair sx={{ fontSize: '3rem' }} />
+                    <Typography component={'h3'} sx={{
+                        fontWeight: 'bold'
+                    }}>
+                        Sofa
+                    </Typography>
+                </Card>
+
+                {/* sofa card  */}
+                <Card sx={{
+                    maxWidth: '8.87rem',
+                    minWidth: '8.5rem',
+                    display: 'flex',
+                    flexDirection: 'column',
+                    alignItems: 'center'
+                }}>
+                    <Chair sx={{ fontSize: '3rem' }} />
+                    <Typography component={'h3'} sx={{
+                        fontWeight: 'bold'
+                    }}>
+                        Sofa
+                    </Typography>
+                </Card>
+
+                {/* sofa card  */}
+                <Card sx={{
+                    maxWidth: '8.87rem',
+                    minWidth: '8.5rem',
+                    display: 'flex',
+                    flexDirection: 'column',
+                    alignItems: 'center'
+                }}>
+                    <Chair sx={{ fontSize: '3rem' }} />
+                    <Typography component={'h3'} sx={{
+                        fontWeight: 'bold'
+                    }}>
+                        Sofa
+                    </Typography>
+                </Card>
+
+                {/* sofa card  */}
+                <Card sx={{
+                    maxWidth: '8.87rem',
+                    minWidth: '8.5rem',
+                    display: 'flex',
+                    flexDirection: 'column',
+                    alignItems: 'center'
+                }}>
+                    <Chair sx={{ fontSize: '3rem' }} />
+                    <Typography component={'h3'} sx={{
+                        fontWeight: 'bold'
+                    }}>
+                        Sofa
+                    </Typography>
+                </Card>
+
+                {/* sofa card  */}
+                <Card sx={{
+                    maxWidth: '8.87rem',
+                    minWidth: '8.5rem',
+                    display: 'flex',
+                    flexDirection: 'column',
+                    alignItems: 'center'
+                }}>
+                    <Chair sx={{ fontSize: '3rem' }} />
+                    <Typography component={'h3'} sx={{
+                        fontWeight: 'bold'
+                    }}>
+                        Sofa
+                    </Typography>
+                </Card>
+
+                {/* sofa card  */}
+                <Card sx={{
+                    maxWidth: '8.87rem',
+                    minWidth: '8.5rem',
+                    display: 'flex',
+                    flexDirection: 'column',
+                    alignItems: 'center'
+                }}>
+                    <Chair sx={{ fontSize: '3rem' }} />
+                    <Typography component={'h3'} sx={{
+                        fontWeight: 'bold'
+                    }}>
+                        Sofa
+                    </Typography>
+                </Card>
+            </Stack>
+        </Container>
+    );
+};
+
+export default Categories;
