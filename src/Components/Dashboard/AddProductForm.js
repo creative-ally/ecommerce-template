108403607d9d88bdec5b74913ab049701cf86ae1
Neamul01 <neamulhaque2002@gmail.com>
2022-11-10 17:04:40 +0600

    product add form component created

---
+++ src/Components/Dashboard/AddProductForm.js
@@ -0,0 +1,12 @@
+import { Paper } from '@material-ui/core';
+import React from 'react';
+
+const AddProductForm = () => {
+    return (
+        <Paper>
+            hello
+        </Paper>
+    );
+};
+
+export default AddProductForm;
