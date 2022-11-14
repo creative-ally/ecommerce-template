e1128069ccd22577dc97eb7a3152faef148afaf5
Md Shohag <mdshohag119764@gmail.com>
2022-11-14 21:11:11 +0600

    produtcs detail init

---
+++ src/SearchProdductsDetail/SearchProdductsDetail.js
@@ -0,0 +1,38 @@
+import React from "react";
+import { useEffect } from "react";
+import { useState } from "react";
+import { useParams } from "react-router-dom";
+
+const SearchProdductsDetail = () => {
+  const { id } = useParams();
+  const [products, setProducts] = useState([]);
+  useEffect(() => {
+    const url = `http://localhost:5000/api/product/${id}`;
+    fetch(url)
+      .then((res) => res.json())
+      .then((data) => setProducts(data.data[0]));
+  }, []);
+  console.log(products);
+
+  return (
+    <div>
+      welcome to detals:{id}
+      <div className="hero min-h-screen bg-base-200">
+        <div className="hero-content flex-col lg:flex-row">
+          <img
+            src={products.image}
+            className="max-w-sm rounded-lg shadow-2xl"
+          />
+          <div>
+            <h1 className="text-4xl font-bold">{products.name}</h1>
+            <p className="py-6">{products.description}</p>
+            <button className="btn btn-primary">Get Started</button>
+          </div>
+        </div>
+      </div>
+      s
+    </div>
+  );
+};
+
+export default SearchProdductsDetail;
--- src/SearchProducts/SearchProducts.js
+++ src/SearchProducts/SearchProducts.js
@@ -1,8 +1,14 @@
 import React from "react";
+import { useNavigate } from "react-router-dom";
 
 const SearchProducts = ({ searchProduct }) => {
-  const { image, name, category, subcategory, description } = searchProduct;
+  const { _id, image, name, category, subcategory, description } =
+    searchProduct;
   console.log(searchProduct);
+  const navigate = useNavigate();
+  const handleProductsDetail = (_id) => {
+    navigate(`/searchPrducts/${_id}`);
+  };
   return (
     <div className="card">
       {/* <div>
@@ -18,7 +24,15 @@ const SearchProducts = ({ searchProduct }) => {
           <p className="text-center w-74">category: {category}</p>
           <p className="text-center  w-74">subcategory: {subcategory}</p>
           <div className="card-actions justify-center">
-            <button className="btn btn-primary">Details</button>
+            <button
+              onClick={() => handleProductsDetail(_id)}
+              className="btn btn-primary"
+            >
+              <label htmlFor="my-modal-5" className="">
+                detals
+              </label>
+            </button>
+            <div className="modal-action"></div>
           </div>
         </div>
       </div>
