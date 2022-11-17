3f4ee074a97a2dad36fe76c4409f122d230a7d3d
Neamul01 <neamulhaque2002@gmail.com>
2022-11-17 23:03:15 +0600

    tahle toolbar separated

--- src/Components/Dashboard/ProductTable.js
+++ src/Components/Dashboard/ProductTable.js
@@ -1,6 +1,4 @@
 import * as React from "react";
-import PropTypes from "prop-types";
-import { alpha } from "@mui/material/styles";
 import Box from "@mui/material/Box";
 import Table from "@mui/material/Table";
 import TableBody from "@mui/material/TableBody";
@@ -8,16 +6,10 @@ import TableCell from "@mui/material/TableCell";
 import TableContainer from "@mui/material/TableContainer";
 import TablePagination from "@mui/material/TablePagination";
 import TableRow from "@mui/material/TableRow";
-import Toolbar from "@mui/material/Toolbar";
-import Typography from "@mui/material/Typography";
 import Paper from "@mui/material/Paper";
 import Checkbox from "@mui/material/Checkbox";
-import IconButton from "@mui/material/IconButton";
-import Tooltip from "@mui/material/Tooltip";
-import DeleteIcon from "@mui/icons-material/Delete";
-import FilterListIcon from "@mui/icons-material/FilterList";
-import TableSorting from "../../Components/Dashboard/TableSorting";
 import EnhancedTableHead from "./Table/EnhancedTableHead";
+import EnhancedTableToolbar from "./Table/EnhancedTableToolbar";
 
 function createData(name, calories, fat, carbs, protein) {
   return {
@@ -74,101 +66,6 @@ function stableSort(array, comparator) {
   });
   return stabilizedThis.map((el) => el[0]);
 }
-
-function EnhancedTableToolbar(props) {
-  const { numSelected } = props;
-  const sortItems = [
-    {
-      lable: "Show by",
-      items: ["12 Row", "24 Row", "36 Row"],
-    },
-    {
-      lable: "rating by",
-      items: ["1 Star", "2 Star", "3 Star", "4 Star", "5 Star"],
-    },
-    {
-      lable: "CATEGORY by",
-      items: ["Mens", "Womens", "Kids"],
-    },
-    {
-      lable: "Brand by",
-      items: ["Lotto", "RFL", "Akij"],
-    },
-  ];
-  return (
-    <Toolbar
-      sx={{
-        pl: { sm: 0 },
-        pr: { xs: 1, sm: 1 },
-        ...(numSelected > 0 && {
-          bgcolor: (theme) =>
-            alpha(
-              theme.palette.primary.main,
-              theme.palette.action.activatedOpacity
-            ),
-        }),
-      }}
-    >
-      {numSelected > 0 ? (
-        <Typography
-          sx={{ flex: "1 1 100%" }}
-          color="inherit"
-          variant="subtitle1"
-          component="div"
-        >
-          {numSelected} selected
-        </Typography>
-      ) : (
-        <Box>
-          <Typography
-            sx={{
-              fontWeight: "700",
-              fontSize: "1.2rem",
-              mt: 3,
-              color: "#403e57",
-              textTransform: "capitalize",
-            }}
-          >
-            Best Selling Products
-          </Typography>
-          <Box
-            sx={{
-              flex: "1 1 100%",
-              display: "flex",
-              flexDirection: "row",
-              justifyContent: "space-between",
-              gap: 1,
-              mb: 3,
-              mt: 1,
-            }}
-          >
-            {sortItems.map((item) => (
-              <TableSorting key={item.lable} item={item} />
-            ))}
-          </Box>
-        </Box>
-      )}
-
-      {numSelected > 0 ? (
-        <Tooltip title="Delete">
-          <IconButton>
-            <DeleteIcon />
-          </IconButton>
-        </Tooltip>
-      ) : (
-        <Tooltip title="Filter list">
-          <IconButton>
-            <FilterListIcon />
-          </IconButton>
-        </Tooltip>
-      )}
-    </Toolbar>
-  );
-}
-
-EnhancedTableToolbar.propTypes = {
-  numSelected: PropTypes.number.isRequired,
-};
 
 export default function EnhancedTable() {
   const [order, setOrder] = React.useState("asc");
---
+++ src/Components/Dashboard/Table/EnhancedTableToolbar.js
@@ -0,0 +1,101 @@
+import { alpha } from "@material-ui/core";
+import { Box, IconButton, Toolbar, Tooltip, Typography } from "@mui/material";
+import PropTypes from "prop-types";
+import DeleteIcon from "@mui/icons-material/Delete";
+import FilterListIcon from "@mui/icons-material/FilterList";
+import TableSorting from "../../../Components/Dashboard/TableSorting";
+
+export default function EnhancedTableToolbar(props) {
+  const { numSelected } = props;
+  const sortItems = [
+    {
+      lable: "Show by",
+      items: ["12 Row", "24 Row", "36 Row"],
+    },
+    {
+      lable: "rating by",
+      items: ["1 Star", "2 Star", "3 Star", "4 Star", "5 Star"],
+    },
+    {
+      lable: "CATEGORY by",
+      items: ["Mens", "Womens", "Kids"],
+    },
+    {
+      lable: "Brand by",
+      items: ["Lotto", "RFL", "Akij"],
+    },
+  ];
+  return (
+    <Toolbar
+      sx={{
+        pl: { sm: 0 },
+        pr: { xs: 1, sm: 1 },
+        ...(numSelected > 0 && {
+          bgcolor: (theme) =>
+            alpha(
+              theme.palette.primary.main,
+              theme.palette.action.activatedOpacity
+            ),
+        }),
+      }}
+    >
+      {numSelected > 0 ? (
+        <Typography
+          sx={{ flex: "1 1 100%" }}
+          color="inherit"
+          variant="subtitle1"
+          component="div"
+        >
+          {numSelected} selected
+        </Typography>
+      ) : (
+        <Box>
+          <Typography
+            sx={{
+              fontWeight: "700",
+              fontSize: "1.2rem",
+              mt: 3,
+              color: "#403e57",
+              textTransform: "capitalize",
+            }}
+          >
+            Best Selling Products
+          </Typography>
+          <Box
+            sx={{
+              flex: "1 1 100%",
+              display: "flex",
+              flexDirection: "row",
+              justifyContent: "space-between",
+              gap: 1,
+              mb: 3,
+              mt: 1,
+            }}
+          >
+            {sortItems.map((item) => (
+              <TableSorting key={item.lable} item={item} />
+            ))}
+          </Box>
+        </Box>
+      )}
+
+      {numSelected > 0 ? (
+        <Tooltip title="Delete">
+          <IconButton>
+            <DeleteIcon />
+          </IconButton>
+        </Tooltip>
+      ) : (
+        <Tooltip title="Filter list">
+          <IconButton>
+            <FilterListIcon />
+          </IconButton>
+        </Tooltip>
+      )}
+    </Toolbar>
+  );
+}
+
+EnhancedTableToolbar.propTypes = {
+  numSelected: PropTypes.number.isRequired,
+};
