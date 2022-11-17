47c52f7b5a101eb2596c1f4e12ec9e341b478ded
Neamul01 <neamulhaque2002@gmail.com>
2022-11-17 22:55:52 +0600

    create another file for table head

--- src/Components/Dashboard/ProductTable.js
+++ src/Components/Dashboard/ProductTable.js
@@ -6,10 +6,8 @@ import Table from "@mui/material/Table";
 import TableBody from "@mui/material/TableBody";
 import TableCell from "@mui/material/TableCell";
 import TableContainer from "@mui/material/TableContainer";
-import TableHead from "@mui/material/TableHead";
 import TablePagination from "@mui/material/TablePagination";
 import TableRow from "@mui/material/TableRow";
-import TableSortLabel from "@mui/material/TableSortLabel";
 import Toolbar from "@mui/material/Toolbar";
 import Typography from "@mui/material/Typography";
 import Paper from "@mui/material/Paper";
@@ -18,8 +16,8 @@ import IconButton from "@mui/material/IconButton";
 import Tooltip from "@mui/material/Tooltip";
 import DeleteIcon from "@mui/icons-material/Delete";
 import FilterListIcon from "@mui/icons-material/FilterList";
-import { visuallyHidden } from "@mui/utils";
 import TableSorting from "../../Components/Dashboard/TableSorting";
+import EnhancedTableHead from "./Table/EnhancedTableHead";
 
 function createData(name, calories, fat, carbs, protein) {
   return {
@@ -77,118 +75,118 @@ function stableSort(array, comparator) {
   return stabilizedThis.map((el) => el[0]);
 }
 
-const headCells = [
-  {
-    id: "name",
-    numeric: false,
-    disablePadding: true,
-    label: "Dessert (100g serving)",
-  },
-  {
-    id: "calories",
-    numeric: true,
-    disablePadding: false,
-    label: "Calories",
-  },
-  {
-    id: "fat",
-    numeric: true,
-    disablePadding: false,
-    label: "Fat (g)",
-  },
-  {
-    id: "carbs",
-    numeric: true,
-    disablePadding: false,
-    label: "Carbs (g)",
-  },
-  {
-    id: "carbs",
-    numeric: true,
-    disablePadding: false,
-    label: "Carbs (g)",
-  },
-  {
-    id: "protein",
-    numeric: true,
-    disablePadding: false,
-    label: "Protein (g)",
-  },
-];
+// const headCells = [
+//   {
+//     id: "name",
+//     numeric: false,
+//     disablePadding: true,
+//     label: "Dessert (100g serving)",
+//   },
+//   {
+//     id: "calories",
+//     numeric: true,
+//     disablePadding: false,
+//     label: "Calories",
+//   },
+//   {
+//     id: "fat",
+//     numeric: true,
+//     disablePadding: false,
+//     label: "Fat (g)",
+//   },
+//   {
+//     id: "carbs",
+//     numeric: true,
+//     disablePadding: false,
+//     label: "Carbs (g)",
+//   },
+//   {
+//     id: "carbs",
+//     numeric: true,
+//     disablePadding: false,
+//     label: "Carbs (g)",
+//   },
+//   {
+//     id: "protein",
+//     numeric: true,
+//     disablePadding: false,
+//     label: "Protein (g)",
+//   },
+// ];
 
-function EnhancedTableHead(props) {
-  const {
-    onSelectAllClick,
-    order,
-    orderBy,
-    numSelected,
-    rowCount,
-    onRequestSort,
-  } = props;
-  const createSortHandler = (property) => (event) => {
-    onRequestSort(event, property);
-  };
+// function EnhancedTableHead(props) {
+//   const {
+//     onSelectAllClick,
+//     order,
+//     orderBy,
+//     numSelected,
+//     rowCount,
+//     onRequestSort,
+//   } = props;
+//   const createSortHandler = (property) => (event) => {
+//     onRequestSort(event, property);
+//   };
 
-  return (
-    <TableHead
-      sx={{
-        background: "linear-gradient(to top, #0858f7, #2b77e5)",
-      }}
-    >
-      <TableRow>
-        <TableCell padding="checkbox">
-          <Checkbox
-            indeterminate={numSelected > 0 && numSelected < rowCount}
-            checked={rowCount > 0 && numSelected === rowCount}
-            onChange={onSelectAllClick}
-            inputProps={{
-              "aria-label": "select all desserts",
-            }}
-            sx={{
-              color: "white",
-            }}
-          />
-        </TableCell>
-        {headCells.map((headCell) => (
-          <TableCell
-            key={headCell.id}
-            align={headCell.numeric ? "right" : "left"}
-            padding={headCell.disablePadding ? "none" : "normal"}
-            sortDirection={orderBy === headCell.id ? order : false}
-            sx={{
-              color: "white",
-            }}
-          >
-            <TableSortLabel
-              active={orderBy === headCell.id}
-              direction={orderBy === headCell.id ? order : "asc"}
-              onClick={createSortHandler(headCell.id)}
-              sx={{
-                color: "white !important",
-              }}
-            >
-              {headCell.label}
-              {orderBy === headCell.id ? (
-                <Box component="span" sx={visuallyHidden}>
-                  {order === "desc" ? "sorted descending" : "sorted ascending"}
-                </Box>
-              ) : null}
-            </TableSortLabel>
-          </TableCell>
-        ))}
-      </TableRow>
-    </TableHead>
-  );
-}
+//   return (
+//     <TableHead
+//       sx={{
+//         background: "linear-gradient(to top, #0858f7, #2b77e5)",
+//       }}
+//     >
+//       <TableRow>
+//         <TableCell padding="checkbox">
+//           <Checkbox
+//             indeterminate={numSelected > 0 && numSelected < rowCount}
+//             checked={rowCount > 0 && numSelected === rowCount}
+//             onChange={onSelectAllClick}
+//             inputProps={{
+//               "aria-label": "select all desserts",
+//             }}
+//             sx={{
+//               color: "white",
+//             }}
+//           />
+//         </TableCell>
+//         {headCells.map((headCell) => (
+//           <TableCell
+//             key={headCell.id}
+//             align={headCell.numeric ? "right" : "left"}
+//             padding={headCell.disablePadding ? "none" : "normal"}
+//             sortDirection={orderBy === headCell.id ? order : false}
+//             sx={{
+//               color: "white",
+//             }}
+//           >
+//             <TableSortLabel
+//               active={orderBy === headCell.id}
+//               direction={orderBy === headCell.id ? order : "asc"}
+//               onClick={createSortHandler(headCell.id)}
+//               sx={{
+//                 color: "white !important",
+//               }}
+//             >
+//               {headCell.label}
+//               {orderBy === headCell.id ? (
+//                 <Box component="span" sx={visuallyHidden}>
+//                   {order === "desc" ? "sorted descending" : "sorted ascending"}
+//                 </Box>
+//               ) : null}
+//             </TableSortLabel>
+//           </TableCell>
+//         ))}
+//       </TableRow>
+//     </TableHead>
+//   );
+// }
 
-EnhancedTableHead.propTypes = {
-  numSelected: PropTypes.number.isRequired,
-  onRequestSort: PropTypes.func.isRequired,
-  onSelectAllClick: PropTypes.func.isRequired,
-  order: PropTypes.oneOf(["asc", "desc"]).isRequired,
-  orderBy: PropTypes.string.isRequired,
-  rowCount: PropTypes.number.isRequired,
-};
+// EnhancedTableHead.propTypes = {
+//   numSelected: PropTypes.number.isRequired,
+//   onRequestSort: PropTypes.func.isRequired,
+//   onSelectAllClick: PropTypes.func.isRequired,
+//   order: PropTypes.oneOf(["asc", "desc"]).isRequired,
+//   orderBy: PropTypes.string.isRequired,
+//   rowCount: PropTypes.number.isRequired,
+// };
 
 function EnhancedTableToolbar(props) {
   const { numSelected } = props;
