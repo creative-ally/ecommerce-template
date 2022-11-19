f08b3c6eaa561fc06f6ce7bb99b52d94094c54fb
Neamul01 <neamulhaque2002@gmail.com>
2022-11-19 15:33:22 +0600

    avatar added in profile

--- src/Pages/Dashboard/UserProfile.js
+++ src/Pages/Dashboard/UserProfile.js
@@ -1,35 +1,67 @@
-import { Box, Grid } from "@mui/material";
+import { Avatar, Paper, Stack } from "@mui/material";
 import React from "react";
 import RouteHeader from "../../Components/Dashboard/RouteHeader";
+import { styled } from "@mui/material/styles";
+import Badge from "@mui/material/Badge";
 
 const UserProfile = () => {
   return (
     <RouteHeader title={"My Profile"}>
-      <Grid container spacing={2} sx={{ m: "0 !important" }}>
-        <Grid container spacing={2} xs={12} md={12} lg={12}>
-          {/* {dashboardCardItem.map((card) => (
-              <ProductCard key={card} card={card} />
-            ))} */}
-        </Grid>
-      </Grid>
-      <Box sx={{ my: 2, P: 2 }}>
-        <Grid
+      <Stack sx={{ my: 2, P: 2, alignItems: "center" }}>
+        <Paper
           container
           spacing={2}
           xs={12}
           md={12}
           lg={12}
-          sx={{ m: "0 !important" }}
+          sx={{ m: "0 !important", width: 610, px: 5, py: 3 }}
         >
-          {/* <ProductTable
-              rows={rows}
-              headCells={headCells}
-              component={"orders"}
-            /> */}
-        </Grid>
-      </Box>
+          <Stack direction="row" spacing={2}>
+            <StyledBadge
+              overlap="circular"
+              anchorOrigin={{ vertical: "bottom", horizontal: "right" }}
+              variant="dot"
+            >
+              <Avatar
+                alt="Remy Sharp"
+                src="/static/images/avatar/1.jpg"
+                sx={{ width: 56, height: 56 }}
+              />
+            </StyledBadge>
+          </Stack>
+        </Paper>
+      </Stack>
     </RouteHeader>
   );
 };
 
 export default UserProfile;
+
+const StyledBadge = styled(Badge)(({ theme }) => ({
+  "& .MuiBadge-badge": {
+    backgroundColor: "#44b700",
+    color: "#44b700",
+    boxShadow: `0 0 0 2px ${theme.palette.background.paper}`,
+    "&::after": {
+      position: "absolute",
+      top: 0,
+      left: 0,
+      width: "100%",
+      height: "100%",
+      borderRadius: "50%",
+      animation: "ripple 1.2s infinite ease-in-out",
+      border: "1px solid currentColor",
+      content: '""',
+    },
+  },
+  "@keyframes ripple": {
+    "0%": {
+      transform: "scale(.8)",
+      opacity: 1,
+    },
+    "100%": {
+      transform: "scale(2.4)",
+      opacity: 0,
+    },
+  },
+}));
