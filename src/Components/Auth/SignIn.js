a78036a17e985c3f2b86ccb7b3c25fb409d6897b
muhammad-jiku <muhammadjiku364@gmail.com>
2022-12-08 12:02:57 +0600

    sign in api is added

--- src/Components/Auth/SignIn.js
+++ src/Components/Auth/SignIn.js
@@ -1,16 +1,17 @@
-import React, { useState } from "react";
-import { Divider, IconButton, InputAdornment, TextField } from "@mui/material";
-import MailOutlineIcon from "@mui/icons-material/MailOutline";
-import HttpsIcon from "@mui/icons-material/Https";
-import { Link, useNavigate } from "react-router-dom";
-import SocialIcons from "./SocialIcons";
+import React, { useEffect, useState } from 'react';
+import { Divider, IconButton, InputAdornment, TextField } from '@mui/material';
+import MailOutlineIcon from '@mui/icons-material/MailOutline';
+import HttpsIcon from '@mui/icons-material/Https';
+import { Link, useLocation, useNavigate } from 'react-router-dom';
+import SocialIcons from './SocialIcons';
 import { Controller, useForm } from 'react-hook-form';
-import { Visibility, VisibilityOff } from "@mui/icons-material";
-import { useSignInWithEmailAndPassword } from "react-firebase-hooks/auth";
-import auth from "../../firebase.init";
+import { Visibility, VisibilityOff } from '@mui/icons-material';
+import { useSignInWithEmailAndPassword } from 'react-firebase-hooks/auth';
+import auth from '../../firebase.init';
 import { yupResolver } from '@hookform/resolvers/yup';
-import { loginSchema } from "./utils/Validation";
-import Loader from "../../Shared/Loader/Loader";
+import { loginSchema } from './utils/Validation';
+import Loader from '../../Shared/Loader/Loader';
+import { toast } from 'react-toastify';
 
 const SignIn = () => {
   const defaultValues = {
@@ -18,42 +19,84 @@ const SignIn = () => {
     password: '',
   };
 
-  const navigate = useNavigate()
   const { control, formState, handleSubmit, reset } = useForm({
     mode: 'onChange',
     defaultValues,
     resolver: yupResolver(loginSchema),
   });
-  const [email, setEmail] = useState('');
-  const [showPassword, setShowPassword] = React.useState(false);
-
-  const [
-    signInWithEmailAndPassword,
-    user,
-    loading,
-  ] = useSignInWithEmailAndPassword(auth);
 
   const { isValid, dirtyFields, errors } = formState;
-  if (loading) {
-    return <Loader />
-  }
-  if (user) {
-    navigate('/')
-  }
 
-  const onSubmit = (values) => {
-    console.log(values)
-    signInWithEmailAndPassword(values.email, values.password)
+  const [signInWithEmailAndPassword, user, loading, signInError] =
+    useSignInWithEmailAndPassword(auth);
 
-  }
+  const navigate = useNavigate();
+  const location = useLocation();
 
+  const [email, setEmail] = useState('');
+  const [signInToken, setSignInToken] = useState('');
+  const [showPassword, setShowPassword] = useState(false);
+
+  let from = location.state?.from?.pathname || '/';
+
+  useEffect(() => {
+    if (signInToken) {
+      console.log(user);
+      navigate(from, { replace: true });
+    }
+  }, [signInToken, user, navigate, from]);
+
+  useEffect(() => {
+    if (signInError) {
+      toast.error('Invalid email or password');
+    }
+    return;
+  }, [signInError]);
+
+  if (loading) return <Loader />;
+
+  const onSubmit = async (values) => {
+    const email = values?.email;
+    const password = values?.password;
+    await signInWithEmailAndPassword(email, password);
+
+    const oldUser = {
+      email: email,
+      password: password,
+    };
+    // sign in method
+    fetch(`http://localhost:5000/api/auth/sign-in`, {
+      method: 'POST',
+      headers: {
+        authorization: `Bearer ${localStorage?.getItem('accessToken')}`,
+        'content-type': 'application/json',
+      },
+      body: JSON.stringify(oldUser),
+    })
+      .then((res) => res.json())
+      .then((data) => {
+        console.log(data);
+        if (data?.insertedId) {
+          // console.log('data inside user token ', data);
+          const accessToken = data?.accessToken;
+          localStorage?.setItem('accessToken', accessToken);
+          setSignInToken(accessToken);
+
+          toast.success('Sign In Successful!');
+          reset();
+        } else {
+          toast.error('Sign In Failed!');
+        }
+      })
+      .catch((err) => console.log(err));
+  };
 
   return (
     <div className="authentication">
       <div className="authentication-content">
         <h1 className="text-[40px] font-bold">Sign in to Your Account</h1>
         <p className="mb-4">Sign in using social networks</p>
-     
+
         <SocialIcons />
         <Divider />
 
@@ -62,11 +105,11 @@ const SignIn = () => {
           noValidate
           autoComplete="off"
           onSubmit={handleSubmit(onSubmit)}
-          className="mt-5">
-
+          className="mt-5"
+        >
           <div className="flex  w-3/4 py-2 rounded-2xl px-2 mb-5 bg-slate-100 mx-auto ">
             <span className="mr-2 text-gray-500 my-auto">
-              {" "}
+              {' '}
               <MailOutlineIcon />
             </span>
 
@@ -90,7 +133,6 @@ const SignIn = () => {
               )}
             />
           </div>
-
 
           <div className="flex justify-between w-3/4 py-2 rounded-2xl px-2 mx-auto bg-slate-100 ">
             <span className="mr-2 text-gray-500 my-auto">
@@ -140,22 +182,14 @@ const SignIn = () => {
             />
           </div>
 
-
           <div className="flex flex-col my-5 mx-auto ">
             <Link to="/forgotpass">
               <span className="mx-auto text-white cursor-pointer mb-5">
                 Forget Password?
               </span>
             </Link>
-            <button className="btn w-1/3 my-4 button mx-auto ">
-              Sign In
-            </button>
-
+            <button className="btn w-1/3 my-4 button mx-auto ">Sign In</button>
           </div>
-
-
-
-
         </form>
 
         {/* <form onSubmit={handleSubmit} className="mt-5 ">
@@ -191,10 +225,12 @@ const SignIn = () => {
         </form> */}
         <div className="mt-6">
           <h1 className="text-white">New Here?</h1>
-          <p className="text-center text-white">Sign up and discover a great amount of new opportunities!</p>
-          <Link to='/signup'><button className="btn w-1/3 mt-4 button">
-            Sign Up
-          </button></Link>
+          <p className="text-center text-white">
+            Sign up and discover a great amount of new opportunities!
+          </p>
+          <Link to="/signup">
+            <button className="btn w-1/3 mt-4 button">Sign Up</button>
+          </Link>
         </div>
       </div>
     </div>
